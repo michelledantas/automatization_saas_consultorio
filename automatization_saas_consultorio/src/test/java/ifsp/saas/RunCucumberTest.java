d30c541f5f53940c18903da4cad05ad7b4f8f7b4
michelledantas <michelledantasluiz@gmail.com>
2025-06-14 01:19:10 -0300

    refactor: RunCucumberTest.java

--- automatization_saas_consultorio/src/test/java/ifsp/saas/RunCucumberTest.java
+++ automatization_saas_consultorio/src/test/java/ifsp/saas/RunCucumberTest.java
@@ -9,6 +9,6 @@ import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ifsp/saas")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ifsp.saas.home,ifsp.saas.loginpaciente,ifsp.saas.loginsecretaria,ifsp.saas.painelsecretaria")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ifsp.saas.home,ifsp.saas.loginpaciente,ifsp.saas.loginsecretaria,ifsp.saas.minhasconsultas,ifsp.saas.painelsecretaria")
 public class RunCucumberTest {
 }
